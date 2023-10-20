59cf54cfb7240f7767dcb35c66d623050c89f9b5
Julien boisgard <julien.boisgard37@gmail.com>
2023-10-20 18:14:15 +0200

    add register page

--- src/Controller/EditController.php
+++ src/Controller/EditController.php
@@ -45,4 +45,34 @@ class EditController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+    
+    #[Route('/register', name: 'app_register')]
+    public function register(Security $security, Request $request,  UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new Agent();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('edit/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
 }
